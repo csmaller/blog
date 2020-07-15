d7c5de861feb2e5f487c8b2fb66026a9bcb3fe28
corey smaller <coreysmaller@gmail.com>
2020-07-14 22:48:31 -0600

    logo, bgimage layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby";
+import { Button } from "react-bootstrap";
 import "../../style.scss";
-
+import HeroImage from "../../content/assets/superior.jpg";
+import Logo from "../../content/assets/jackalopelogo.jpg";
 import styled from "styled-components"
 
-import { rhythm, scale } from "../utils/typography"
+import { Container } from "react-bootstrap";
 
 const LayoutContainer = styled.div`
-
   margin-left: auto;
   margin-right: auto;
 `;
@@ -17,63 +18,57 @@ export default function Layout(props) {
   const { location, title, children, background } = props
   const rootPath = `${__PATH_PREFIX__}/`
   const blogPath = `${__PATH_PREFIX__}/blog/`
-  let header
+  const header = (
+    <Link
+      style={{
+        boxShadow: `none`,
+        textDecoration: `none`,
+        color: `inherit`,
+      }}
+      to={location.pathname === blogPath ? `/` : `/`}
+    >
+      <img src={Logo} class="float-left" />
+      <h1 className="mt-4 ml-5 float-left" >{title}</h1>
+    </Link>
+  )
 
- // if (location.pathname === rootPath || location.pathname === blogPath) {
-    header = (
-      <h1 className="mt-1" >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={location.pathname === blogPath ? `/` : `/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  // } else {
-  //   header = (
-  //     <h3
-  //       style={{
-  //         fontFamily: `Montserrat, sans-serif`,
-  //         marginTop: 0,
-  //       }}
-  //     >
-  //       <Link
-  //         style={{
-  //           boxShadow: `none`,
-  //           textDecoration: `none`,
-  //           color: `inherit`,
-  //         }}
-  //         to={`/blog/`}
-  //       >
-  //         {title}
-  //       </Link>
-  //     </h3>
-  //   )
-  // }
   return (
-    <Wrapper >
-      <LayoutContainer >
-        <div className="row d-flex justify-content-center">
-          <header className="col-12">{header}</header>
-        </div>
-        <div className="row">
-          <main>{children}</main>
-        </div>
-      </LayoutContainer>
-      <Footer className="row">
-        © {new Date().getFullYear()}, Built with
+    <Container>
+      <Wrapper >
+        <LayoutContainer >
+          <div className="row d-flex justify-content-center">
+            <Header className="col-12 mb-0">{header}</Header>
+            <nav className="col-12 mb-4 mt-0 justify-content-left">
+              <Link to="/blog/">
+                <Button marginTop="35px">Blog</Button>
+              </Link>
+            </nav>
+          </div>
+          <img src={HeroImage} className="w-100 h-50 p-0" />
+          <div className="row">
+            <main>{children}</main>
+          </div>
+        </LayoutContainer>
+        <Footer className="align-content-center">
+          © {new Date().getFullYear()}, Built with
           {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </Footer>
-    </Wrapper>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </Footer>
+      </Wrapper>
+    </Container>
   )
 
 }
+
+const Header = styled.div`
+   img { 
+     width: 100px;
+   }
+`
+
+const Nav = styled.div`
+
+`;
 
 const Wrapper = styled.div`
   min-height: 100vh;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
 
-class IndexPage extends React.Component {
-  render() {
-    const siteTitle = "Corey Smaller"
+export default function IndexPage(props) {
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`jackalope adventures`, `jackalope`, `backcountry skiing`, `utah ski touring`]}
-        />
-       
-        <h1>
-          Welcome to Jackalope Adventures
-        </h1>
-       
-        <Link to="/blog/">
-          <Button marginTop="35px">Blog</Button>
-        </Link>
-      </Layout>
-    )
-  }
+  const siteTitle = "Jackalope Adventures"
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title="Home"
+        keywords={[`jackalope adventures`, `jackalope`, `backcountry skiing`, `utah ski touring`]}
+      />
+
+      <h1>
+        Welcome!
+      </h1>
+
+    </Layout>
+  );
+
 }
-
-export default IndexPage
