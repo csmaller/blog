c1cb88e82b96a1d74f68a8363b801129f91ab074
corey smaller <coreysmaller@gmail.com>
2020-07-18 15:27:46 -0600

    add social media class and main Hero Image for inded page. clean up nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,57 +2,54 @@ import React from "react"
 import { Link } from "gatsby";
 import { Button } from "react-bootstrap";
 import "../../style.scss";
-import HeroImage from "../../content/assets/superior.jpg";
-import Logo from "../../content/assets/jackalopelogo.jpg";
-import styled from "styled-components"
+import Logo from "../../content/assets/logo_no_bg.png";
+import styled from "styled-components";
+import { SocialIcon } from 'react-social-icons'
 
 import { Container } from "react-bootstrap";
-
-const LayoutContainer = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-`;
 
 export default function Layout(props) {
 
   const { location, title, children, background } = props
   const rootPath = `${__PATH_PREFIX__}/`
   const blogPath = `${__PATH_PREFIX__}/blog/`
-  const header = (
-    <Link
-      style={{
-        boxShadow: `none`,
-        textDecoration: `none`,
-        color: `inherit`,
-      }}
-      to={location.pathname === blogPath ? `/` : `/`}
-    >
-      <img src={Logo} class="float-left" />
-      <h1 className="mt-4 ml-5 float-left" >{title}</h1>
-    </Link>
-  )
+
+  function Header() {
+    return (
+      <>
+        <div className="col-1">
+          <Link to={location.pathname === blogPath ? `/` : `/`} >
+            <img src={Logo} class="float-left" />
+          </Link>
+        </div>
+        <nav className="col-11 mb-4 mt-0 justify-content-left bg-primary mt-4 p-0">
+          <Link to="/" disabled={location.pathname === "/"}>
+            <Button marginTop="35px">Home</Button>
+          </Link>
+          <Link to="/blog/" disabled={location.pathname === "/blog/"}>
+            <Button marginTop="35px">Blog</Button>
+          </Link>
+        </nav>
+      </>
+    );
+  }
+
 
   return (
     <Container>
       <Wrapper >
         <LayoutContainer >
-          <div className="row d-flex justify-content-center">
-            <Header className="col-12 mb-0">{header}</Header>
-            <nav className="col-12 mb-4 mt-0 justify-content-left">
-              <Link to="/blog/">
-                <Button marginTop="35px">Blog</Button>
-              </Link>
-            </nav>
-          </div>
-          <img src={HeroImage} className="w-100 h-50 p-0" />
+          <HeaderContainer className="row mb-0">
+            <Header />
+          </HeaderContainer>
           <div className="row">
             <main>{children}</main>
           </div>
         </LayoutContainer>
         <Footer className="align-content-center">
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          Follow us on Social Media
+          <SocialIcon url="https://www.facebook.com/jackalopeAdventureTouring"/>
+          <SocialIcon url="https://www.instagram.com/jackalopeadventures/" />
         </Footer>
       </Wrapper>
     </Container>
@@ -60,11 +57,16 @@ export default function Layout(props) {
 
 }
 
-const Header = styled.div`
-   img { 
-     width: 100px;
-   }
-`
+const LayoutContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const HeaderContainer = styled.div`
+  nav{
+    height: 40px;
+  }
+`;
 
 const Nav = styled.div`
 
@@ -75,8 +77,16 @@ const Wrapper = styled.div`
 `
 
 const Footer = styled.footer`
-  text-align: center;
+  text-align: left;
+  margin:auto;
   margin: 24px;
+  position:absolute;
+  bottom:0;
+
+  a{
+    box-shadow: none;
+    margin-left:10px;
+  }
 `
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,69 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import styled from "styled-components";
+import HeroImage from "../../content/assets/superior_rectangle.png";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Button from "../components/button"
 
+
+const HeroContainer = styled.div`
+  background-size: cover;
+  padding: 1px 0px 0px 0px;
+  position: relative;
+
+  .container-hero-image-background {
+    width: 100%;
+    height: 100%;
+    top: 100px;
+    position: absolute;
+
+    .hero-image-source {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+`;
+
+const MainContainer = styled.div`
+  .overlay {
+    width: 50%;
+    padding: 75px 95px 75px 70px;
+    opacity: .9;
+    color: white;
+  }
+`;
+
+function MainContent() {
+  return (
+    
+      <div id="divOverlay" data-overlay-display="True"  class="overlay bg-primary">
+        <h2 class="heading2 content white" id="why_mountain_america_3">Welcome to Jackalope Adventures</h2>
+      </div>
+  )
+
+}
 export default function IndexPage(props) {
 
   const siteTitle = "Jackalope Adventures"
 
   return (
-    <Layout location={props.location} title={siteTitle}>
-      <SEO
-        title="Home"
-        keywords={[`jackalope adventures`, `jackalope`, `backcountry skiing`, `utah ski touring`]}
-      />
+    <>
+      <HeroContainer>
+        <div className="container-hero-image-background">
+          <img src={HeroImage} className="hero-image-sournce" />
+        </div>
+      </HeroContainer>
 
-      <h1>
-        Welcome!
-      </h1>
-
-    </Layout>
+      <Layout location={props.location} title={siteTitle}>
+        <SEO
+          title="Home"
+          keywords={[`jackalope adventures`, `jackalope`, `backcountry skiing`, `utah ski touring`]}
+        />
+        <MainContainer>
+          <MainContent />
+        </MainContainer>
+      </Layout>
+    </>
   );
 
 }
